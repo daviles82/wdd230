ecbe374b089f583c122fe6eea7156fa8e3c3e44f
daviles82 <avileshouston@live.com>
2023-01-12 04:27:28 -0600

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 const h1 = document.querySelector('h1');
-const copyright = document.querySelector('#copyright');
 
 h1.textContent = `Brother David Aviles`;
-const currentYear = new Date().getFullYear();
-copyright.textContent = `©️ ${new Date().getFullYear()}`;
 let quantity = document.querySelector('#q').value;
+
+
+document.getElementById("modifiedtime").innerHTML = new Date(document.lastModified);
+
+
+
