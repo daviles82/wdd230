b34d422b7488ae77a6f3f87b1b840a51897b7d83
Daviles82 <avileshouston@live.com>
2023-04-25 20:15:11 -0500

    requested update

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,6 @@
 document.getElementById("modifiedtime").innerHTML = new Date(document.lastModified);
 
+
+const currentyear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentyear;
