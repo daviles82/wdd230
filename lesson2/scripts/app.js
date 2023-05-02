d0ba314b2313327e9f2a8d6e8548ab33febdbc51
Daviles82 <avileshouston@live.com>
2023-05-01 20:27:58 -0500

    week 2 lesson

---
+++ lesson2/scripts/app.js
@@ -0,0 +1,3 @@
+document.getElementById("modifiedtime").innerHTML = new Date(document.lastModified);
+const currentyear = new Date().getFullYear();
+document.querySelector("#currentyear").textContent = currentyear;
