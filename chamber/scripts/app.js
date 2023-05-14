546cfa32504a8d2dbf5c9a8e34d4645a8c887472
Daviles82 <avileshouston@live.com>
2023-05-14 14:04:29 -0500

    time debug update

---
+++ chamber/scripts/app.js
@@ -0,0 +1,12 @@
+document.getElementById("modifiedtime").innerHTML = new Date(
+  document.lastModified
+);
+const currentyear = new Date().getFullYear();
+document.querySelector("#currentyear").textContent = currentyear;
+
+const options = { weekday: 'long', day: 'numeric' , month: 'long', year: 'numeric' };
+const today = new Date();
+const current = today.toLocaleDateString("en-US", options);
+
+console.log(document.querySelector("time"))
+document.querySelector("time").textContent = current
