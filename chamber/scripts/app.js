f0af8e6ea93ed7f5726deced9cea8da4de9c9954
Daviles82 <avileshouston@live.com>
2023-05-14 23:31:18 -0500

    week 4 assignment

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -6,7 +6,16 @@ document.querySelector("#currentyear").textContent = currentyear;
 
 const options = { weekday: 'long', day: 'numeric' , month: 'long', year: 'numeric' };
 const today = new Date();
-const current = today.toLocaleDateString("en-US", options);
+const current = today.toLocaleDateString("en-UK", options);
 
 console.log(document.querySelector("time"))
 document.querySelector("time").textContent = current
+
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
