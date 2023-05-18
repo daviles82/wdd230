69144781c4d3d53b7f383ed5e32b4d977fe1440c
Daviles82 <avileshouston@live.com>
2023-05-17 21:50:04 -0500

    assignment

---
+++ dommanipulation/scripts/bom.js
@@ -0,0 +1,38 @@
+// make sure the input is not blank before doing the following remaining tasks in this list
+// create an li element 
+// create a delete button 
+// populate the li elements textContent or innerHTML with the input 
+// populate the button textContent with an ❌ 
+// append the li element with the delete button 
+// append the list element with the li element just created and appended with text and the delete button YYYY
+// add an event listener to the delete button that removes the li element when clicked
+// send the focus to the input element
+// change the input value to nothing or the empty string to clean up the interface for the user
+
+
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const list = document.querySelector('#list')
+
+button.addEventListener('click', () => {
+    const myChapter = input.value;
+    input.value = '';
+
+    const listChapters = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listChapters.appendChild(listText);
+    listText.textContent = myChapter;
+    listChapters.appendChild(listBtn);
+    listBtn.textContent = 'X';
+    list.appendChild(listChapters);
+
+  
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listChapters);
+    });
+
+    input.focus();
+  })
