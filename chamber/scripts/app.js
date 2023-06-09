c7c545755777afe356f74e39e28d9907457f7eea
Daviles82 <avileshouston@live.com>
2023-06-08 23:18:43 -0500

    week 7 assignment

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -4,13 +4,17 @@ document.getElementById("modifiedtime").innerHTML = new Date(
 const currentyear = new Date().getFullYear();
 document.querySelector("#currentyear").textContent = currentyear;
 
-const options = { weekday: 'long', day: 'numeric' , month: 'long', year: 'numeric' };
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
 const today = new Date();
 const current = today.toLocaleDateString("en-UK", options);
 
-console.log(document.querySelector("#time"))
-document.querySelector("#time").textContent = current
-
+console.log(document.querySelector("#time"));
+document.querySelector("#time").textContent = current;
 
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
@@ -20,11 +24,90 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
-
 // banner
 const today2 = new Date();
 const dayOfWeek = today.getDay();
 
 if (dayOfWeek === 1 || dayOfWeek === 2) {
-  document.getElementById("montuebanner").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  document.getElementById("montuebanner").innerHTML =
+    "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
+
+const image = {
+  threshold: 0,
+  rootMargin: "0px 0px 10px 0px",
+};
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, image);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+
+function getDaysSinceLastVisit() {
+  // Get the current date and time.
+  const now = new Date();
+
+  // Get the day last visited from local storage.
+  const dayLastVisited = window.localStorage.getItem("dayLastVisited");
+
+  // If the day last visited is not set, return 0.
+  if (!dayLastVisited) {
+    return 0;
+  }
+
+  // Calculate the number of days since the last visit.
+  const daysSinceLastVisit = Math.round((now - new Date(dayLastVisited)) / (1000 * 60 * 60 * 24));
+
+  // Return the number of days since the last visit.
+  return daysSinceLastVisit;
+}
+
+// Update the time between visits display.
+function updateTimeBetweenVisits() {
+  const daysSinceLastVisit = getDaysSinceLastVisit();
+  document.getElementById("time-between-visits").innerHTML = daysSinceLastVisit;
+}
+
+// Set the day last visited to the current day when the page loads.
+window.addEventListener("load", () => {
+  window.localStorage.setItem("dayLastVisited", new Date().toISOString());
+  updateTimeBetweenVisits();
+});
