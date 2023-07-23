e602bae763a93ced1423c30d01d4cb4c70bc1a17
Daviles82 <avileshouston@live.com>
2023-07-22 20:58:32 -0500

    update

--- project/scripts/fresh.js
+++ project/scripts/fresh.js
@@ -1,3 +1,9 @@
+const lastModified = new Date(document.lastModified);
+const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+document.getElementById("modifiedtime").innerHTML = lastModified.toLocaleString('en-US', options);
+
+
+
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("menuBtn").classList.toggle("open");
