87ce7a66b2fe3e73a89535bd2f6ae27a0971529c
Daviles82 <avileshouston@live.com>
2023-07-22 21:15:42 -0500

    update

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -80,3 +80,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const x = document.getElementById("menuBtn");
   x.onclick = toggleMenu;
 });
+
+
+const image = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, image);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
